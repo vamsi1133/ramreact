da2ee9bc87bbd2bfc1adf2778486e39cff51485d
vamsi krishna raju pericherla <vamsiraju113@gmail.com>
2023-02-06 11:26:22 +0530

    fourth commit

--- src/Components/login/index.js
+++ src/Components/login/index.js
@@ -1,37 +1,144 @@
 import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline";
+import TextField from "@mui/material/TextField";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Checkbox from "@mui/material/Checkbox";
+import Link from "@mui/material/Link";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+function Copyright(props) {
+  return (
+    <Typography
+      variant="body2"
+      color="text.secondary"
+      align="center"
+      {...props}
+    >
+      {"Copyright © "}
+      <Link color="inherit" href="https://mui.com/">
+        Your Website
+      </Link>{" "}
+      {new Date().getFullYear()}
+      {"."}
+    </Typography>
+  );
+}
 
+const theme = createTheme();
 const Login = (props) => {
   const [credential, setCredentials] = useState({ username: "", password: "" });
-
+  const router = useNavigate();
   const handleInput = (e) => {
     const { value, name } = e.target;
     setCredentials((prev) => ({ ...prev, [name]: value }));
   };
 
-  const handleLogin = (event) => {
+  const handleSubmit = async (event) => {
     event.preventDefault();
-    console.log(credential);
+    // fetch("http://localhost:8000/login", {
+    //   method: "POST",
+    //   headers: {
+    //     "Content-Type": "application/json",
+    //   },
+    //   body: JSON.stringify(credential),
+    // })
+    //   .then((res) => res.json())
+    //   .then((data) => {
+    //     if (!data.ok) {
+    //       alert("incorect data");
+    //     }
+    //   })
+    //   .catch((err) => console.log("here", err));
+    try {
+      const data = await axios.post("http://localhost:8000/login", credential);
+      if (data.status === 200) {
+        router("/");
+      }
+    } catch (e) {
+      alert(e.response.data.message);
+    }
   };
 
   return (
     <>
-      <div>
-        <h1>Login Page</h1>
-        <form onChange={handleInput}>
-          <input
-            name="username"
-            placeholder="username"
-            value={credential.username}
-          ></input>
-          <input
-            name="password"
-            type="password"
-            placeholder="password"
-            value={credential.password}
-          ></input>
-          <button onClick={handleLogin}>Login</button>
-        </form>
-      </div>
+      <ThemeProvider theme={theme}>
+        <Container component="main" maxWidth="xs">
+          <CssBaseline />
+          <Box
+            sx={{
+              marginTop: 8,
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+            }}
+          >
+            <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}></Avatar>
+            <Typography component="h1" variant="h5">
+              Sign in
+            </Typography>
+            <Box
+              component="form"
+              onSubmit={handleSubmit}
+              onChange={handleInput}
+              noValidate
+              sx={{ mt: 1 }}
+            >
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                id="email"
+                label="Username"
+                name="username"
+                autoComplete="email"
+                autoFocus
+              />
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                name="password"
+                label="Password"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+              />
+              <FormControlLabel
+                control={<Checkbox value="remember" color="primary" />}
+                label="Remember me"
+              />
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                sx={{ mt: 3, mb: 2 }}
+              >
+                Sign In
+              </Button>
+              <Grid container>
+                <Grid item xs>
+                  <Link href="#" variant="body2">
+                    Forgot password?
+                  </Link>
+                </Grid>
+                <Grid item>
+                  <Link href="#" variant="body2">
+                    {"Don't have an account? Sign Up"}
+                  </Link>
+                </Grid>
+              </Grid>
+            </Box>
+          </Box>
+          <Copyright sx={{ mt: 8, mb: 4 }} />
+        </Container>
+      </ThemeProvider>
     </>
   );
 };
